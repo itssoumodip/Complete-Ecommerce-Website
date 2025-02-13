4cb65d658f52024d7c2e5e77d1b614a5746dc867
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-13 12:38:51 +0000

    creating the nav

---
+++ nikeWebsite/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import {headerLogo} from '../assets/images'
+
+function Nav() {
+  return (
+    <header>
+        <nav>
+            <a href="/">
+                <img src="{headerLogo}"/>
+            </a>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
