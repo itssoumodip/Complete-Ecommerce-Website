fff44d1d691e4ae5c71e53bdfcece1c9db80c3d3
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-13 16:50:05 +0000

    making the nav bar

--- nikeWebsite/src/components/Nav.jsx
+++ nikeWebsite/src/components/Nav.jsx
@@ -1,13 +1,22 @@
 import {headerLogo} from '../assets/images'
 import {hamburger} from '../assets/icons'
-
+import {navLinks} from '../constants'
 function Nav() {
   return (
-    <header>
-        <nav>
+    <header className='padding-x py-8 abosolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
             <a href="/">
                 <img src={headerLogo} alt="Logo" width={130} height={29}/>
             </a>
+            <ul className='flex-1 flex justify-center gap-16 max-lg:hidden'>
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                  {item.label}
+                </a>
+                </li>
+              ))}
+            </ul>
         </nav>
     </header>
   )
