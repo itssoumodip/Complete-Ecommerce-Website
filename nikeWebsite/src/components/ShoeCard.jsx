6bb68fa69b6c05deaa4d30668ca964416c0ac805
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-17 18:55:44 +0000

    shoecard error fixing

--- nikeWebsite/src/components/ShoeCard.jsx
+++ nikeWebsite/src/components/ShoeCard.jsx
@@ -1,6 +1,16 @@
 function ShoeCard({imgURL, changeBigShoeImage, bigShoeImage}) {
+  const handleClick = () => {
+    if(bigShoeImage !== imgURL.bigShoe) 
+      changeBigShoeImage(imgURL.bigShoe);
+  }
   return (
-    <div className={`border-2 rounded-xl`}></div>
+    <div className={`border-2 rounded-xl
+      ${bigShoeImage===imgURL
+        ? 'border-red-400' : 'border-transparent' 
+      } cursor-pointer max-sm:flex-1
+      `}
+      onClick={handleClick}
+      ></div>
   )
 }
 
