7f02610f6724577b61a41bbdf58c16ba328592c2
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-18 19:28:20 +0000

    making the popular product section

--- nikeWebsite/src/components/PopularProductCard.jsx
+++ nikeWebsite/src/components/PopularProductCard.jsx
@@ -1,9 +1,14 @@
-
+import { star } from "../assets/icons"
 function PopularProductCard({name, imgURL, price}) {
   return (
-    <div className="flex flex-1 flex-col w-full max-sm:w-full max-sm:ml-[8%] ">
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
         <img src={imgURL} alt={name} width={280} height={280} />
-        <div className="mt-8 flex justify-start gap-2.5"></div>
+        <div className="mt-8 flex justify-start gap-2.5">
+            <img src={star} alt="rating" width={24} height={24} />
+            <p className="text-xl leading-normal text-gray-500">(4.5)</p>
+        </div>
+          <h3 className="mt-2 text-2xl leading-normal font-semibold">{name}</h3>
+          <p className="mt-2 font-semibold text-red-400 text-2xl leading-normal">{price}</p>
     </div>
   )
 }
--- nikeWebsite/src/sections/Hero.jsx
+++ nikeWebsite/src/sections/Hero.jsx
@@ -5,13 +5,13 @@ import { shoes, statistics } from "../constants"
 import ShoeCard from "../components/ShoeCard"
 import { useState } from "react"
 
-function Hero() {
+function Hero() { 
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 border-3 border-black p-14">
         <p className="text-xl text-red-400">Our Summer Colection</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
           <span className="text-red-400 inline-block mt-3">Nike</span>
@@ -19,7 +19,7 @@ function Hero() {
         </h1>
         <p className="font-montserrat text-gray-600 text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arivals, quality comfort, and innovation for your active life.</p>
         <Button label = "Shop Now" iconURL = {arrowRight}/>
-        <div className="flex border justify-start items-start flex-wrap w-full mt-20 gap-16"> 
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16"> 
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className="text-4xl font-palanquin font-bold">{stat.value} </p> 
