30f01bb115f3228c17e65d04323a938d672c570f
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-18 18:18:11 +0000

    popular products card design making

--- nikeWebsite/src/sections/PopularProducts.jsx
+++ nikeWebsite/src/sections/PopularProducts.jsx
@@ -3,7 +3,9 @@ import { products } from "../constants";
 function PopularProducts() {
   return (
     <section id="products" className="max-container max-sm:mt-12">
-          
+        <div className="flex flex-col justify-start gap-5 border-3"></div>
+        <h2>Popular Products</h2>
+        <p>Experience top-notch quality and style with our sought-after selections. Discover a World of comphort, design, and value</p>
     </section>
   )
 }
