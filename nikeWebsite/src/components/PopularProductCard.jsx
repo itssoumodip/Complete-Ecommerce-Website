733f967eeeaf7feb6f02f620d590506d928e73fe
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-18 19:48:33 +0000

    fixing the errors

--- nikeWebsite/src/components/PopularProductCard.jsx
+++ nikeWebsite/src/components/PopularProductCard.jsx
@@ -1,7 +1,7 @@
 import { star } from "../assets/icons"
 function PopularProductCard({name, imgURL, price}) {
   return (
-    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+    <div className="flex-1 flex-col w-full max-sm:w-full">
         <img src={imgURL} alt={name} width={280} height={280} />
         <div className="mt-8 flex justify-start gap-2.5">
             <img src={star} alt="rating" width={24} height={24} />
