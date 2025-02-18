4f37b6591ef9783d969672686d0c39bf87ad398a
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-18 18:32:29 +0000

    popular product done

--- nikeWebsite/src/sections/PopularProducts.jsx
+++ nikeWebsite/src/sections/PopularProducts.jsx
@@ -4,8 +4,16 @@ function PopularProducts() {
   return (
     <section id="products" className="max-container max-sm:mt-12">
         <div className="flex flex-col justify-start gap-5 border-3"></div>
-        <h2>Popular Products</h2>
-        <p>Experience top-notch quality and style with our sought-after selections. Discover a World of comphort, design, and value</p>
+        <h2 className="text-4xl font-palanquin font-bold">Our 
+          <span className="text-red-400"> Popular </span>
+          Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat">Experience top-notch quality and style with our sought-after selections. Discover a World of comphort, design, and value</p>
+        <div className="mt-16 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 sm:gap-4 gap-14">
+          {products.map((product) => (
+            <PopularProductCard key = 
+            {product.name} {...product} />
+          ))}
+        </div>
     </section>
   )
 }
