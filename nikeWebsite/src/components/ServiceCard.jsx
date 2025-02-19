a66b6cb22f7b17adcbca7ad5049813cc021361b8
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-19 19:28:45 +0000

    refactor PopularProducts and Services sections; add ServiceCard component for improved structure

---
+++ nikeWebsite/src/components/ServiceCard.jsx
@@ -0,0 +1,7 @@
+function ServiceCard({imgURL, label, subtext}) {
+  return (
+    <div className="flex-1">ServiceCard</div>
+  )
+}
+
+export default ServiceCard
--- nikeWebsite/src/sections/Services.jsx
+++ nikeWebsite/src/sections/Services.jsx
@@ -1,7 +1,12 @@
-
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
 function Services() {
   return (
-    <div>Services</div>
+    <section className="border-4 max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
