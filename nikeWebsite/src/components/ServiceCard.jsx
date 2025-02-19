6f12971a1c0df36fd8378c410ad4675036d4de37
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-19 19:49:15 +0000

    enhance ServiceCard component; add image and text elements for improved presentation

--- nikeWebsite/src/components/ServiceCard.jsx
+++ nikeWebsite/src/components/ServiceCard.jsx
@@ -1,6 +1,12 @@
 function ServiceCard({imgURL, label, subtext}) {
   return (
-    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-2xl px-10 py-16">ServiceCard</div>
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-2xl px-10 py-16">
+        <div className="w-11 h-11 flex justify-center items-center bg-red-400 rounded-full">
+            <img src={imgURL} alt={label} width={24} height={24} />
+        </div>
+        <h3 className="mt-5 text-3xl leading-normal font-bold">{label}</h3>
+        <p className="mt-3 break-words text-lg leading-normal text-gray-500">{subtext}</p>
+    </div>
   )
 }
 
